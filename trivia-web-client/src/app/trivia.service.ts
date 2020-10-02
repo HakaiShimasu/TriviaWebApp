47248457dca716d7a6e37114746031f80c6c3532
Vincenzo Vitale <vincenzo9397@gmail.com>
2020-10-02 16:45:47 +0200

    create a service for a trivia

---
+++ trivia-web-client/src/app/trivia.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const TRIVIA_ENDPOINT: string = 'http://localhost:8000/question';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TriviaService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuestion() {
+    return this.http.get(TRIVIA_ENDPOINT);
+  }
+}
